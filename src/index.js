70208fa3e2a9c491e14a6a4a97b7aa82d8bc840a
Austin Smith <atylersmith@outlook.com>
2023-08-29 17:48:30 -0400

    organized files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './css/index.css';
 import './pages/homePage.css'
 import reportWebVitals from './reportWebVitals';
 import Header from "./components/HeaderComponent";
